1fb9b979f1e9a32bb1ea3b08b76ced7400066eb1
Muninn <hyacinthus@gmail.com>
2021-03-30 17:39:40 +0800

    write token to etc file

--- go.mod
+++ go.mod
@@ -8,4 +8,5 @@ require (
 	go.uber.org/multierr v1.6.0 // indirect
 	go.uber.org/zap v1.16.0
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
 )
--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"os"
 	"os/exec"
 	"os/signal"
 	"strings"
@@ -11,11 +14,13 @@ import (
 	"github.com/hack-fan/skadigo"
 	"github.com/jinzhu/configor"
 	"go.uber.org/zap"
+	"gopkg.in/yaml.v3"
 )
 
 // global
 var log *zap.SugaredLogger
 var settings = new(Settings)
+var etcFiles = []string{"skadi.yml", "/etc/skadi/skadi.yml"}
 
 // Setting will be load from /etc/skadi/skadi.yml
 type Settings struct {
@@ -89,23 +94,83 @@ func run(cmd, dir string) (string, error) {
 }
 
 func main() {
-	_ = configor.Load(settings, "skadi.yml", "/etc/skadi/skadi.yml")
-	if settings.Token == "" {
-		panic("token is required")
-	}
+	// load config, user config in workdir has high priority
+	_ = configor.Load(settings, etcFiles...)
 
 	// logger
-	var log *zap.Logger
+	var logger *zap.Logger
 	var err error
 	if settings.Debug {
-		log, err = zap.NewDevelopment()
+		logger, err = zap.NewDevelopment()
 	} else {
-		log, err = zap.NewProduction()
+		logger, err = zap.NewProduction()
 	}
 	if err != nil {
 		panic(err)
 	}
-	defer log.Sync() // nolint
+	defer logger.Sync() // nolint
+	log = logger.Sugar()
+
+	// if there is one arg, set it as token, then exit.
+	if len(os.Args) == 2 {
+		token := os.Args[1]
+		if len(token) != 20 {
+			log.Fatalf("invalid token: %s", token)
+		}
+		var changed bool
+		for _, fname := range etcFiles {
+			f, err := ioutil.ReadFile(fname)
+			if err != nil {
+				continue
+			}
+			var node yaml.Node
+			err = yaml.Unmarshal(f, &node)
+			if err != nil {
+				log.Fatalf("parse etc file [%s] failed: %s", fname, err)
+			}
+			tmp, _ := json.MarshalIndent(node, "", "  ")
+			log.Debug(string(tmp))
+			if node.Content == nil {
+				log.Fatalf("bad etc file [%s]", fname)
+			}
+			var found bool
+			for _, item := range node.Content[0].Content {
+				// token key found in last loop
+				if found {
+					item.Style = yaml.DoubleQuotedStyle
+					item.Tag = "!!str"
+					item.Value = token
+					break
+				}
+				// find the key, change value in next loop
+				if item.Value == "token" {
+					found = true
+				}
+			}
+			if err != nil {
+				log.Fatalf("add token to etc file [%s] failed: %s", fname, err)
+			}
+			nf, err := yaml.Marshal(&node)
+			if err != nil {
+				log.Fatalf("add token to etc file [%s] failed: %s", fname, err)
+			}
+			err = ioutil.WriteFile(fname, nf, 0644)
+			if err != nil {
+				log.Fatalf("write token to etc file [%s] failed: %s", fname, err)
+			}
+
+			changed = true
+		}
+		if !changed {
+			log.Fatal("skadi.yml not found")
+		}
+		os.Exit(0)
+	}
+
+	// check token
+	if settings.Token == "" {
+		log.Fatal("token is required")
+	}
 
 	// system signals - for graceful shutdown or restart
 	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
@@ -113,7 +178,7 @@ func main() {
 
 	// skadi agent
 	agent := skadigo.New(settings.Token, settings.Server, &skadigo.Options{
-		Logger: log.Sugar(),
+		Logger: log,
 	})
 	log.Info("Skadi agent start")
 	// blocked
